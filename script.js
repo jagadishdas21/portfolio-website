ebcb11f0da1a6eabf81ba789de00196c6d27e3b3
Jagadish Das <94641073+jagadishdas21@users.noreply.github.com>
2024-10-15 07:58:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-function toggleMenu() {
-  const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(".hamburger-icon");
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
-}
+const modeToggle = document.getElementById('modeToggle');
+modeToggle.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+    if (document.body.classList.contains('dark-mode')) {
+        modeToggle.textContent = 'Light Mode';
+    } else {
+        modeToggle.textContent = 'Dark Mode';
+    }
+});
+    document.getElementById('sendEmailLink').addEventListener('click', function() {
+        const message = document.getElementById('message').value;
+        const mailtoLink = `mailto:jagadishdas.nitrkl@gmail.com?subject=Inquiry&body=${encodeURIComponent(message)}`;
+        this.href = mailtoLink;
+});
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+
+toggleSwitch.addEventListener('change', () => {
+    document.body.classList.toggle('dark-mode');
+});
